4631ab21684c311b711572b3c03bec13e7b1f008
susanna00 <77734618+susanna00@users.noreply.github.com>
2021-03-08 01:15:33 -0500

    Compiling request URLs

--- app/robo_project.py
+++ app/robo_project.py
@@ -17,6 +17,11 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 def to_usd(my_price):
     return"${0:,.2f}".format(my_price)
 
+# Compiling request URLs
+url_lookup = ""
+def compile_url(ticker_input):
+    url_lookup = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker_input}&apikey={api_key}"
+    return url_lookup
 #
 # INFO OUTPUTS
 #
@@ -25,7 +30,7 @@ def to_usd(my_price):
 
 symbol = "IBM" # accept user input 
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+request_url = 
 
 response = requests.get(request_url)
 
