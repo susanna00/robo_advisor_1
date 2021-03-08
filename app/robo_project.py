a349a99ef6153be4666c44f72dbf8ac4bdc4544a
susanna00 <77734618+susanna00@users.noreply.github.com>
2021-03-08 00:38:50 -0500

    Issue a request

--- app/robo_project.py
+++ app/robo_project.py
@@ -1,5 +1,24 @@
 # app/robo_advisor.py file 
 
+import requests 
+import json 
+
+#
+# INFO OUTPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) # <class 'requests.models.Response'>
+# print(response.status_code) # 200
+# print(response.text) 
+
+parsed_response = json.loads(response.text)
+
+breakpoint()
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
