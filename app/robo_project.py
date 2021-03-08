b3d5347675f9f4fc158fd1e4904c751b40075669
susanna00 <77734618+susanna00@users.noreply.github.com>
2021-03-08 00:42:23 -0500

    Adding latest closest price

--- app/robo_project.py
+++ app/robo_project.py
@@ -3,6 +3,9 @@
 import requests 
 import json 
 
+def to_usd(my_price):
+    return"${0:,.2f}".format(my_price)
+
 #
 # INFO OUTPUTS
 #
@@ -10,13 +13,12 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)
-# print(type(response)) # <class 'requests.models.Response'>
-# print(response.status_code) # 200
-# print(response.text) 
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2021-03-05"]["4. close"]
 
 # breakpoint()
 
@@ -28,7 +30,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
